9926d802df49ec33df47f0c6f680d9ecbc18cde4
JaquelineRocio <jaquelineramosvargas@gmail.com>
2022-12-29 23:52:54 -0500

    Implementando la vista de addpagos

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddPagoComponent } from './add-pago/add-pago.component';
 import { DashboardComponent } from './dashboard.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { ServiciosComponent } from './servicios/servicios.component';
 
 const routes: Routes = [{ path: '', component: DashboardComponent,
 children:[
+  {
+    path:'inicio',
+    component: InicioComponent
+  },
   {
     path:'add-pago',
     component: AddPagoComponent
